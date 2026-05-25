9de66b47a22bb549f499907687a98a508b6c1631
cri4life <thekarimm12@gmail.com>
2026-05-25 15:55:08 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String mainPage() {
+        return "Welcome to my Spring Boot app";
+    }
+
+    @GetMapping("/greeting")
+    public String showGreetingPage() {
+        return "greeting";
+    }
+}
